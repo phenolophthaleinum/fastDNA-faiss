6752e850302c14671814854f680645b168be0429
hyperscroll <14911686+phenolophthaleinum@users.noreply.github.com>
2021-10-20 12:30:41 +0200

    creating random-phylum fasta file for fastDNA training
    Signed-off-by: hyperscroll <mccv99@gmail.com>

--- [fastDNA learning] random-filtering.py
+++ [fastDNA learning] random-filtering.py
@@ -2,6 +2,9 @@ import json
 import secrets
 from collections import defaultdict
 from timeit import default_timer as timer
+from Bio.Seq import Seq
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
 
 # timeit
 start = timer()
@@ -18,12 +21,19 @@ for host in host_data:
 
 # random sampling of a single host from a phylum
 random_phylum_host = {phylum: secrets.choice(phylum_host[phylum]) for phylum in phylum_host}
-# for phylum in phylum_host:
-#     random_phylum_host[phylum] = secrets.choice(phylum_host[phylum])
 print(random_phylum_host)
 
 # list of filenames to be used
 filenames = list(random_phylum_host.values())
+print(len(filenames))
+
+# parse selected files and merge them into single fasta file
+records = [list(SeqIO.parse(f"D:/praktyki2020/edwards2016/host/fasta/{file}.fna", "fasta"))[0] for file in filenames]
+print(len(records))
+print(records)
+
+SeqIO.write(records, "D:/praktyki2020/edwards2016/host/random_phylum-training_fastDNA.fasta", "fasta")
+print(f"Written {len(records)} records.")
 
 end = timer()
 runtime = end - start
