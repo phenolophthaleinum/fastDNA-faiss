09062278583b0ca1fbce0ec752464003cf139bf9
Maciej Michalczyk <mccv99@gmail.com>
2021-10-22 12:38:00 +0200

    added parallelism, random input sampling for embedding, updated readme

---
+++ random-input-sampling.py
@@ -0,0 +1,102 @@
+import json
+import secrets
+import glob
+from timeit import default_timer as timer
+from Bio.Seq import Seq
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from joblib import Parallel, delayed
+from contextlib import ExitStack
+import multiprocessing
+import itertools
+
+
+def sample_sequences(file, length, n):
+    #print(file)
+    record = SeqIO.read(file, "fasta")
+    #print(record)
+    # if record.seq is None:
+    #     print(f"{record.id} has none seq")
+    #print(f"{record.id} : {len(record.seq)}")
+    #samples = []
+    with open("X:/edwards2016/host/random_samples-training_fastDNA.fasta", "a") as w_fh:
+        for i in range(n):
+            pick = secrets.choice(range(0, len(record.seq) - length))
+            #new_seq = record.seq[pick:pick + length]
+        #     #samples.append(str(record.seq[pick:pick + length]))
+            desc = f'{record.description} sample_{i} gen_pos:({pick}:{pick + length})'
+            new_record = SeqRecord(
+                id=record.id,
+                seq=Seq(record.seq[pick:pick + length]),
+                description=desc
+            )
+            SeqIO.write(new_record, w_fh, "fasta")
+    #print(f"{record.id}: {samples}")
+
+
+def main():
+    start = timer()
+
+    # importing json data about hosts
+    with open("host.json", "r") as fh:
+        host_data = json.load(fh)
+
+    # debugging --------------------------------------------------------------------------------------------------------
+    #print(os.scandir("X:/edwards2016/host/fasta/"))
+    #print(len(glob.glob("X:/edwards2016/host/fasta/*.fna")))
+    #print(glob.glob("X:/edwards2016/host/fasta/*.fna"))
+    # debugging --------------------------------------------------------------------------------------------------------
+
+    # parallel sampling records of all files and dumping them into one file
+    par = Parallel(n_jobs=-1)(delayed(sample_sequences)(file, 200, 10) for file in glob.glob("X:/edwards2016/host/fasta/*.fna"))
+
+    # mapping samples to taxid and dumping them into a file
+    p_records = list(SeqIO.parse("X:/edwards2016/host/random_samples-training_fastDNA.fasta", "fasta"))
+    ids_records = [record.id.split(".")[0] for record in p_records]
+    #print(ids_records)
+    d = {}
+    # for id in ids_records:
+    #     index = ids_records.index(id)
+    #     d[index] = id
+    for id in set(ids_records):
+        keys = [index for index, value in enumerate(ids_records) if value == id]
+        for key in keys:
+            d[key] = host_data[id]["taxid"]
+    #print(d)
+    with open("X:/edwards2016/host/sample_map.json", "w", encoding='utf-8') as fh:
+        json.dump(d, fh, indent=4)
+
+    # potential map reading
+    # with open("X:/edwards2016/host/sample_map.json", "r") as fh:
+    #     map_data = {int(key): value for key, value in json.load(fh).items()}
+    # map_data[10000]
+
+    # debugging (can be skipped)---------------------------------------------------------------------------------------------
+
+    #print(json.dumps(d, sort_keys=True))
+    # debugging purposes
+    # from collections import Counter
+    # print(Counter(ids_records))
+
+    # with multiprocessing.Pool(processes=8) as pool:
+    #     pool.starmap(sample_sequences, zip(glob.glob("X:/edwards2016/host/fasta/*.fna"), itertools.repeat(200), itertools.repeat(10)))
+
+    # pool = multiprocessing.Pool(8)
+    # pool.starmap(sample_sequences, zip(glob.glob("X:/edwards2016/host/fasta/*.fna"), itertools.repeat(200), itertools.repeat(10)))
+    # pool.close()
+    # pool.join()
+
+
+    # for file in glob.glob("X:/edwards2016/host/fasta/*.fna"):
+    #     record = SeqIO.read(file, "fasta")
+    #     print(record.id)
+
+    # debugging --------------------------------------------------------------------------------------------------------
+
+    end = timer()
+    runtime = end - start
+    print(f"Done in {runtime:.6f}")
+
+
+if __name__ == "__main__":
+    main()
--- random-phylum-filtering.py
+++ random-phylum-filtering.py
@@ -7,44 +7,65 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 from joblib import Parallel, delayed
 
-# timeit
-start = timer()
+#records = []
 
-# importing json data about hosts
-with open("host.json", "r") as fh:
-    host_data = json.load(fh)
 
-# filtering all hosts by phylum
-phylum_host = defaultdict(list)
-for host in host_data:
-    phylum = host_data[host]["lineage_names"][1]
-    phylum_host[phylum].append(host)
-
-# random sampling of a single host from a phylum
-random_phylum_host = {phylum: secrets.choice(phylum_host[phylum]) for phylum in phylum_host}
-print(random_phylum_host)
-
-# list of filenames to be used
-filenames = list(random_phylum_host.values())
-print(len(filenames))
-
-# get list of taxid (labels in fastDNA)
-labels = [host_data[host]["taxid"] for host in filenames]
-print(labels)
-
-# parse selected files and merge them into single fasta file; create labels file
-records = [list(SeqIO.parse(f"D:/praktyki2020/edwards2016/host/fasta/{file}.fna", "fasta"))[0] for file in filenames]
-print(len(records))
-print(records)
-
-with open("D:/praktyki2020/edwards2016/host/random_phylum-training_labels.txt", "w") as fh:
-    for label in labels:
+def fasta_parallel(file, label):
+    record = SeqIO.read(file, "fasta")
+    with open("X:/edwards2016/host/random_phylum-training_fastDNA.fasta", "a") as w_fh:
+        SeqIO.write(record, w_fh, "fasta")
+    #records.append(record)
+    with open("X:/edwards2016/host/random_phylum-training_labels.txt", "w") as fh:
         fh.write(label + "\n")
 
-#par = Parallel(n_jobs=-1)(delayed(SeqIO.write)(records, "D:/praktyki2020/edwards2016/host/random_phylum-training_fastDNA.fasta", "fasta") for record in records)
-SeqIO.write(records, "D:/praktyki2020/edwards2016/host/random_phylum-training_fastDNA.fasta", "fasta")
-print(f"Written {len(records)} records.")
 
-end = timer()
-runtime = end - start
-print(f"Done in {runtime:.6f}")
+def main():
+    # timeit
+    start = timer()
+
+    # importing json data about hosts
+    with open("host.json", "r") as fh:
+        host_data = json.load(fh)
+
+    # filtering all hosts by phylum
+    phylum_host = defaultdict(list)
+    for host in host_data:
+        phylum = host_data[host]["lineage_names"][1]
+        phylum_host[phylum].append(host)
+
+    # random sampling of a single host from a phylum
+    random_phylum_host = {phylum: secrets.choice(phylum_host[phylum]) for phylum in phylum_host}
+    print(random_phylum_host)
+
+    # list of filenames to be used
+    filenames = list(random_phylum_host.values())
+    print(len(filenames))
+
+    # get list of taxid (labels in fastDNA)
+    labels = [host_data[host]["taxid"] for host in filenames]
+    print(len(labels))
+
+    # parse selected files and merge them into single fasta file; create labels file
+    #records = [list(SeqIO.parse(f"D:/praktyki2020/edwards2016/host/fasta/{file}.fna", "fasta"))[0] for file in filenames]
+    for file, label in zip(filenames, labels):
+        print(f"{file} - {label}")
+
+    par = Parallel(n_jobs=-1)(delayed(fasta_parallel)(f"X:/edwards2016/host/fasta/{file}.fna", label) for file, label in zip(filenames, labels))
+    # print(len(records))
+    # print(records)
+
+    # with open("D:/praktyki2020/edwards2016/host/random_phylum-training_labels.txt", "w") as fh:
+    #     for label in labels:
+    #         fh.write(label + "\n")
+
+    #par = Parallel(n_jobs=-1)(delayed(SeqIO.write)(records, "D:/praktyki2020/edwards2016/host/random_phylum-training_fastDNA.fasta", "fasta") for record in records)
+    #SeqIO.write(records, "X:/edwards2016/host/random_phylum-training_fastDNA.fasta", "fasta")
+    #print(f"Written {len(records)} records.")
+
+    end = timer()
+    runtime = end - start
+    print(f"Done in {runtime:.6f}")
+
+
+if __name__ == "__main__":
+    main()
