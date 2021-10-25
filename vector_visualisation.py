0cdead82e68d9ef9e6adc0f6712917f6b4567acd
Maciej Michalczyk <mccv99@gmail.com>
2021-10-25 13:47:39 +0200

    added sample names creation in vector visualisation
    Signed-off-by: Maciej Michalczyk <mccv99@gmail.com>

--- vector_visualisation.py
+++ vector_visualisation.py
@@ -13,19 +13,23 @@ principals_df = pd.DataFrame(data=principals, columns=["principal_x", "principal
 with open("sample_map.json", "r") as fh:
     map_data = {int(key): value for key, value in json.load(fh).items()}
 principals_df["ncbi_id"] = principals_df.index.map(map_data)
+principals_df_grouped = principals_df.groupby("ncbi_id")
+principals_df["sample_name"] = principals_df["ncbi_id"] + " sample_" + principals_df_grouped.cumcount().map(str)
+
 print(principals_df)
+print(principals_df["sample_name"])
 #print(principals_df.index.tolist())
+#principals_df.to_csv("edwards_vectors.csv", sep=",")
 
 fig = express.scatter_3d(principals_df,
                          x="principal_x", y="principal_y", z="principal_z",
                          color="ncbi_id",
-                         text='ncbi_id',
                          size=principals_df.index.tolist(),
                          size_max=100,
-                         hover_name="ncbi_id",
+                         hover_name="sample_name",
                          hover_data=["principal_x",
                                      "principal_y",
                                      "principal_z",
-                                     principals_df.index.tolist()])
+                                     "ncbi_id"])
 fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
 fig.write_html("edwards_vectors.html")
