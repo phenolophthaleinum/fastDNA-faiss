f23929586dc3ac3c1e31ef8274e92e0dc8c63294
hyperscroll <14911686+phenolophthaleinum@users.noreply.github.com>
2021-11-08 22:06:12 +0100

    a note to a script
    Signed-off-by: hyperscroll <mccv99@gmail.com>

--- taxid2ncbi.py
+++ taxid2ncbi.py
@@ -1,5 +1,9 @@
 import utils
 
+# ten skrypt nie dziala do konca dobrze, co jest niestety wina ncbi albo Roba Edwardsa, a nie ma sensu juz tego meczyć.
+# chodzi o to, ze pewne ncbi_id maja w host.json te same taxid, co powoduje, ze w wyniku tego skryptu pewne organizmy sa
+# zduplikowane mimo, ze we wlasciwym pliku wygenerowany (tym do trenowania) ich nie ma
+
 host_data = utils.get_host_data()
 
 with open("D:/edwards2016/host/random_family-training_labels_test.txt", "r") as fd:
