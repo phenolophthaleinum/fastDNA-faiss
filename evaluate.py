2f2cf532ad54fb4e6273f219660a589620cabf8d
Maciej Michalczyk <mccv99@gmail.com>
2021-11-21 12:31:39 +0100

    Possible boolean matrix
    Signed-off-by: Maciej Michalczyk <mccv99@gmail.com>

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,10 @@
 import argparse
 import json
 from pathlib import Path
+import pandas as pd
+from bokeh.models import BasicTicker, ColorBar, LinearColorMapper, PrintfTickFormatter
+from bokeh.plotting import figure, show
+from bokeh.sampledata.unemployment1948 import data
 
 fh = open('X:/edwards2016/runs/run-dim_60-len_125-n_600-epoch_2-k_7-none_Slim/rank/rank-k_60.json')
 db = json.load(fh)
@@ -38,17 +42,22 @@ def compare_taxranks(vd, hd, rank):
 
 
 d = {}
+d2 = {}
 for vid, hids in db.items():
     d[vid] = []
+    d2[vid] = []
     for taxlevel in TAX_RANKS:
         match = False
         for tup in hids[:1]:
             hid = tup[0]
+            #print(hid)
             match = compare_taxranks(vjson[vid], hjson[hid], taxlevel)
             if match:
                 break
         d[vid].append(match)
-
+        tax_index = hjson[hid]['lineage_ranks'].index(taxlevel)
+        d2[vid].append((hjson[hid]['lineage_names'][tax_index], vjson[vid]["host"]["lineage_names"][tax_index], match))
+print(d2)
 l = [0 for _i in TAX_RANKS]
 for vid in d:
     for i, taxlevel in enumerate(TAX_RANKS):
@@ -61,3 +70,30 @@ for i, tax_rank in enumerate(TAX_RANKS):
     d[tax_rank] = l[i] / len(vjson) * 100
 
 print(d)
+print(data)
+data['Year'] = data['Year'].astype(str)
+data = data.set_index('Year')
+data.drop('Annual', axis=1, inplace=True)
+data.columns.name = 'Month'
+print(data)
+df = pd.DataFrame(data.stack(), columns=['rate']).reset_index()
+print(df)
+d2pd = pd.DataFrame.from_dict(d2, orient='index', columns=list(TAX_RANKS))
+d2pd.index.name = "Virus"
+d2pd.columns.name = "Tax level"
+taxranks = list(d2pd.columns)
+viruses = list(d2pd.index)
+print(d2pd)
+TOOLS = "hover,save,pan,box_zoom,reset,wheel_zoom"
+p = figure(title="fastDNA-faiss predictions matches",
+           x_range=viruses, y_range=taxranks,
+           x_axis_location="above", sizing_mode='stretch_both',
+           tools=TOOLS, toolbar_location='below',
+           tooltips=[('predicted', '@Tax level')])
+colors = ["#75968f", "#550b1d"]
+mapper = LinearColorMapper(palette=colors, low=True, high=False)
+p.rect(x="Virus", y="Tax level", width=1, height=1,
+       source=d2pd,
+       fill_color={'field': 'rate', 'transform': mapper},
+       line_color=None)
+show(p)
