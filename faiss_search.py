24f4585b382dd4938eb0b192ac9091fddea71cac
hyperscroll <14911686+phenolophthaleinum@users.noreply.github.com>
2021-12-03 09:33:38 +0100

    Refactored faiss_search.py
    Signed-off-by: Maciej Michalczyk <mccv99@gmail.com>

--- faiss_search.py
+++ faiss_search.py
@@ -8,118 +8,67 @@ import faiss
 import json
 import argparse
 from joblib import Parallel, delayed
+from joblib.externals.loky import set_loky_pickler
+from joblib import parallel_backend
+from joblib import Parallel, delayed
+from joblib import wrap_non_picklable_objects
 from timeit import default_timer as timer
 from colorama import Fore, init
 
-# import holoviews as hv
-# from holoviews import dim, opts
-# dim = 10
-# k_nearest = 10
-# query = np.loadtxt("/home/hyperscroll/fastDNA/virus_vectors.txt", dtype="float32")
-# index = faiss.read_index("/home/hyperscroll/fastDNA/host_index.index")
-# with open("/home/hyperscroll/edwards2016/host/sample_map.json", "r") as fh:
-#     map_data = {int(key): value for key, value in json.load(fh).items()}
-# distances, indices = index.search(query, k_nearest)
-# print(f"Nearest sequences: {indices}")
-# classification = np.vectorize(map_data.get)(indices)
-# print(f"Classification: {classification}")
-#
-# pre_rank = defaultdict(list)
-# for index, distance in np.stack((classification.flatten(), distances.flatten()), axis=1):
-#     pre_rank[index].append(2 - float(distance))
-# sum_rank = {key: sum(values) for key, values in pre_rank.items()}
-# final_rank = dict(sorted(sum_rank.items(), key=lambda elem: elem[1], reverse=True))
-# print(json.dumps(final_rank, indent=4))
 
-# TODO prepraracja przykladu [-1,..., -1] x [1, ..., 1] dim=60 do faissa i dac surowe dystanse
-# TODO wyszukac blad z ucinaniem jsona
-# test paralelizmu nadal
-def do_search(file: str, dim: int, n_samples: int, k_nearest: str, index: object, map_data: Dict[int, str]) -> Dict[str, List[Tuple[str, float]]]:
-    # with txt file;
-    # query = np.loadtxt(file, dtype="float32")
-    # dim = np.shape(query)[1]
-    # with binary:
+# @delayed
+@wrap_non_picklable_objects
+def do_search(file: str, dim: int, n_samples: int, k_nearest: int, index: faiss.IndexFlatL2,
+              map_data: Dict[int, str]) -> Dict[str, List[Tuple[str, float]]]:
+    """
+    Args:
+        file (str): path to a binary file with query vectors (virus)
+        dim (int): dimensionality of a vector
+        n_samples (int): number of samples that query file contains (number of vectors)
+        k_nearest (int): k-nearest vectors to be found
+        index (IndexFlatL2): faiss index object to be searched
+        map_data (dict[int, str]): dictionary describing index - genome_name relations
+
+    Returns:
+        dict: dictionary of viruses with assigned list of the nearest hosts ranked by summed up scores (sum of scores 0-1 for each matched query sample) in a descending order
+    """
     query = np.fromfile(file, dtype="float32")
-    #n_host = query.size / (n_samples * dim)
     query = query.reshape(n_samples, dim)
-    hyper = 2 * math.sqrt(dim)
-    #index = faiss.read_index(faiss_index)
-    distances, indices = index.search(query, int(k_nearest))
-    #distances, indices = faiss_index.search(query, int(k_nearest))
+    distances, indices = index.search(query, k_nearest)
     classification = np.vectorize(map_data.get)(indices)
-    #ranks = []
     coef = 1 / (4 * dim)
     pre_rank = defaultdict(list)
-    # print(f"Max dist: {distances.max()}")
-    # print(f"Min dist: {distances.min()}")
-    # print(np.stack((classification.flatten(), distances.flatten()), axis=1))
-    # print(np.stack((classification.flatten(), distances.flatten()), axis=1).shape[0])
-    current_sample = 0
     enum = 1
     dict_sample = defaultdict(list)
-    dict_data = defaultdict(list)
-    # no filtering
-    # for idx, distance, in np.stack((classification.flatten(), distances.flatten()), axis=1):
-    #     #ranks.append((index, 2 - float(distance))) # this is wrong
-    #     # 2 - dist
-    #     #pre_rank[index].append(2 - float(distance))
-    #     # hyper - dist
-    #     # no sqrt on distance
-    #     #score = hyper - float(distance)
-    #     # sqrt on dist
-    #     if enum % dim == 0:
-    #         current_sample += 1
-    #     print(f"Sample: {current_sample}")
-    #     score = hyper - math.sqrt(float(distance))
-    #     pre_rank[idx].append(score)
-    #     if score < 0:
-    #         print(f"Negative score: {score}; virus, host: {('_'.join(file.split('/')[-1].split('.')[0].split('_')[:2]), idx)}; distance: {distance}\n")
-    #     # if score >= 0:
-    #     #     pre_rank[index].append(score)
-    #     # else:
-    #     #     raise ValueError(f"Negative score: {score}; index: {index}; distance: {distance}")
-    #     enum += 1
-    # filtering
-    for idx, distance, in np.stack((classification.flatten(), distances.flatten()), axis=1):
+    # print(f"Distances row number (number of samples): {np.shape(distances)[0]}")
+    # print(f"Distances col number (number of k-nearest):{np.shape(distances)[1]}")
+    # print(f"Indices row number (number of samples){np.shape(indices)[0]}")
+    # print(f"Indices col number (number of k-nearest){np.shape(indices)[1]}")
+    # print(f"Max dist: {distances.max()}")
+    # print(f"Min dist: {distances.min()}")
+    # print(len(np.stack((classification.flatten(), distances.flatten()), axis=1)))
 
-        #ranks.append((index, 2 - float(distance))) # this is wrong
-        # 2 - dist
-        #pre_rank[index].append(2 - float(distance))
-        # hyper - dist
-        # no sqrt on distance
-        #score = hyper - float(distance)
-        # sqrt on dist
-        #score = hyper - math.sqrt(float(distance))
-        #score = 1 - math.sqrt(float(distance))
+    for idx, distance, in np.stack((classification.flatten(), distances.flatten()), axis=1):
         score = float(distance) * coef
         if score < 0:
-            print(f"Negative score: {score}; virus, host: {('_'.join(file.split('/')[-1].split('.')[0].split('_')[:2]), idx)}; distance: {distance}\n")
+            print(
+                f"Negative score: {score}; virus, host: {('_'.join(file.split('/')[-1].split('.')[0].split('_')[:2]), idx)}; distance: {distance}\n")
         dict_sample[idx].append(score)
-        if (enum % dim == 0):
+        if enum % dim == 0:
             for host in dict_sample:
                 pre_rank[idx].append(max(dict_sample[host]))
-                    # dictionary = {f"Host: {h}": hv.Scatter(scores) for h, scores in dict_data.items()}
-                    # hv.HoloMap(dictionary, kdims="Host")
+            # print(f"Dict_sample size: {len(dict_sample)}")
             dict_sample.clear()
-        if "_".join(file.split("/")[-1].split(".")[0].split("_")[:2]) == "NC_000866" and enum == 36000:
-            # dictionary = {f"Host: {h}": hv.Scatter(scores) for h, scores in dict_data.items()}
-            # hv.HoloMap(dictionary, kdims="Host")
-            with open("NC_000866_plot_data.json", "w") as f:
-                json.dump(pre_rank, f)
-        # if score >= 0:
-        #     pre_rank[index].append(score)
-        # else:
-        #     raise ValueError(f"Negative score: {score}; index: {index}; distance: {distance}")
         enum += 1
 
     sum_rank = {key: sum(values) for key, values in pre_rank.items()}
     ranks = sum_rank.items()
-    part = {"_".join(file.split("/")[-1].split(".")[0].split("_")[:2]): sorted(ranks, key=lambda elem: elem[1], reverse=True)}
+    part = {"_".join(file.split("/")[-1].split(".")[0].split("_")[:2]): sorted(ranks, key=lambda elem: elem[1],
+                                                                               reverse=True)}
     return part
-    #global_rank.update(part)
 
 
-def run_procedure(input_dir, output, k_nearest, dim, n_samples, faiss_index, map):
+def run_procedure(input_dir: str, output: str, k_nearest: int, dim: int, n_samples: int, faiss_index: str, map: str):
     # colorama
     init()
 
@@ -128,11 +77,11 @@ def run_procedure(input_dir, output, k_nearest, dim, n_samples, faiss_index, map
     with open(map, "r") as fh:
         map_data = {int(key): value for key, value in json.load(fh).items()}
     index = faiss.read_index(faiss_index)
+    print(type(index))
+    # set_loky_pickler("pickle")
     ranks = Parallel(verbose=True, n_jobs=-1, prefer="threads")(
-            delayed(do_search)(file, dim, n_samples, k_nearest, index, map_data) for file in glob.glob(f"{input_dir}*.vec"))
-    # txt version
-    # ranks = Parallel(verbose=True, n_jobs=-1)(
-    #     delayed(do_search)(file, k_nearest, index, map_data) for file in glob.glob(f"{input_dir}*.txt"))
+        delayed(do_search)(file, dim, n_samples, k_nearest, index, map_data) for file in glob.glob(f"{input_dir}*.vec"))
+
     for result in ranks:
         global_rank.update(result)
     with open(output, "w") as fd:
@@ -141,12 +90,6 @@ def run_procedure(input_dir, output, k_nearest, dim, n_samples, faiss_index, map
     end = timer()
     runtime = end - start
     print(f"{Fore.GREEN} Done in {runtime:.6f} seconds")
-
-#input: /home/hyperscroll/edwards2016/virus/vectors/
-#output: virus_rank.json
-#index: /home/hyperscroll/fastDNA/host_index_100.index
-#map: /home/hyperscroll/edwards2016/host/sample_map_100.json
-# python faiss_search.py -i /home/hyperscroll/edwards2016/virus/vectors/ -o virus_rank.json -k 10 -f /home/hyperscroll/fastDNA/host_index_100.index -m /home/hyperscroll/edwards2016/host/sample_map_100.json
 
 
 if __name__ == "__main__":
@@ -169,4 +112,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     global_rank = {}
-    run_procedure(args.input_dir, args.output, args.k_nearest, int(args.dim), int(args.n_samples), args.faiss_index, args.map)
+    run_procedure(args.input_dir, args.output, int(args.k_nearest), int(args.dim), int(args.n_samples),
+                  args.faiss_index, args.map)
