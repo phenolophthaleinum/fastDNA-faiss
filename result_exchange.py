75476e41973847e832d878fb2a0f703b318c9a9c
hyperscroll <mccv99@gmail.com>
2022-03-30 17:13:23 +0200

    result exchange debug script
    Signed-off-by: hyperscroll <mccv99@gmail.com>

---
+++ result_exchange.py
@@ -0,0 +1,12 @@
+import utils
+import json
+import pandas
+import pandas as pd
+
+
+with open("bayes_test_avg.json", 'r') as fh:
+    res_json = json.load(fh)
+virus_data = utils.get_virus_data()
+hostname_data = utils.get_hostname_data()
+new_d = {(virus_data[k]["organism_name"], k): (hostname_data[v[0][0]]["lineage_names"], v[0][1]) for k, v in res_json.items()}
+df = pd.DataFrame.from_dict(new_d, orient="index")
