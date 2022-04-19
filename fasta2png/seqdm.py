5fe96a8cdb2ba128ec1f59c274d3af57b5f1c389
hyperscroll <mccv99@gmail.com>
2022-04-19 17:12:32 +0200

    sneaky fasta 2 png
    Signed-off-by: phenolophthaleinum <mccv99@gmail.com>

--- fasta2png/seqdm.py
+++ fasta2png/seqdm.py
@@ -56,8 +56,10 @@ from timeit import default_timer as timer
 # square sized img
 total_start = timer()
 # seq_obj, = SeqIO.parse("X:/edwards2016/virus/fasta/NC_000866.fna", 'fasta')
-seq_obj, = SeqIO.parse("X:/edwards2016/caenorhabditis_elegans.fna", 'fasta')
-p_seq = dill.dumps(seq_obj)
+# seq_obj, = SeqIO.parse("X:/edwards2016/caenorhabditis_elegans.fna", 'fasta')
+seq_obj, = SeqIO.parse("X:/edwards2016/virus/fasta/NC_007461.fna", 'fasta')
+# p_seq = dill.dumps(str(seq_obj.seq))
+p_seq = str.encode(str(seq_obj.seq))
 # with open("NC_dilled.dill", 'wb') as f:
 #     dill.dump(seq_obj, f, protocol=dill.HIGHEST_PROTOCOL)
 # dim = int(math.sqrt(len(p_seq)))
@@ -67,7 +69,7 @@ diff = target - len(p_seq)
 n_seq = p_seq + bytes(diff)
 dim = int(math.sqrt(len(n_seq)))
 img = Image.frombytes('P', (dim, dim), n_seq)
-img.save(f"caenorhabditis_elegans_sq.png")
+img.save(f"NC_007461_v_seq2.png")
 total_end = timer()
 total_runtime = total_end - total_start
 print(f"Total elapsed time (SeqRecord save to png file): {total_runtime:.6f} seconds")
